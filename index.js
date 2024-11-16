ceef32b43dd2d1460cf076dcd95e42ddd3b9ff19
savyagupta-2004 <guptasavya667@gmail.com>
2024-11-16 17:28:58 +0530

    chnages for cors

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const server = http.createServer(app);
 
 // Allow all origins temporarily
 const corsOptions = {
-  origin: "*",
+  origin: "https://fixmybug.me",
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
